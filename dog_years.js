1836dd1129d675211cac6e3ac77c50ede811656f
Solomon Owusu Sarfo <87814663+JedSolo@users.noreply.github.com>
2022-01-15 13:17:11 +0000

    Dog Years

---
+++ dog_years.js
@@ -0,0 +1,20 @@
+// Assigning a variable to my age:
+let myAge = 25;
+
+let earlyYears = 2;
+earlyYears *= 10.5;
+
+// Substracting 2 from my age:
+let laterYears = myAge - 2;
+
+// Multiplying the later years by 4:
+laterYears *= 4;
+
+//Calculating my age in Dog years:
+let myAgeInDogYears = earlyYears + laterYears;
+
+// Converting my name to lowercase:
+let myName = 'SOLOMON'.toLowerCase();
+
+// Print out my age in dog years:
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
